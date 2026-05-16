6877feec4fda906ea7005b8d4b237817d4729247
abdul-korim-web <moneyschorsing@gmail.com>
2026-05-16 22:58:35 +0600

    versel setup

--- index.js
+++ index.js
@@ -85,6 +85,8 @@ app.use((err,req,res,next)=>{
 
 
 const port = process.env.PORT || 5000
-app.listen(port,()=>{
-    console.log(`server running on port ${port}`);
-})
+// app.listen(port,()=>{
+//     console.log(`server running on port ${port}`);
+// })
+// for versel 
+export default app;
